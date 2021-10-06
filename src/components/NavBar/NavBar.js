8eef463c99475ffd4509e7f800c3343966ff1ad8
ManuLosta <manulostalo@hotmail.com.ar>
2021-10-06 11:20:17 -0300

    Added navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,21 @@
+import logo from '../../assets/icons/logo.svg';
+import './NavBar.scss';
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <nav className="NavBar">
+      <div className="NavBar__logo">
+        <img src={logo} alt="Logo" />
+        <h1>CoffeeCommerce</h1>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li>Productos</li>
+        <li>Carrito</li>
+      </ul>
+    </nav>
   );
 };
 

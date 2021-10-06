81172103afe04bc8d413bee83011d1b60d373b2e
ManuLosta <manulostalo@hotmail.com.ar>
2021-10-05 23:36:03 -0300

    removed unused files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Bienvenid@ a Coffeecommerce!!</h1>
     </div>
   );

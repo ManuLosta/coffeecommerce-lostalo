3a2e80ea55dc6063e1474fd7505248231b3ff3cc
ManuLosta <manulostalo@hotmail.com.ar>
2021-10-24 18:55:07 -0300

    fixed gh-pages routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import NavBar from './components/NavBar/NavBar';
 import Home from './pages/Home';
